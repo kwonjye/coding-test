605463296d14b489d475e2710db37884900da0d2
KWONJYE <sheisdanmi@gmail.com>
2022-08-04 09:11:09 +0900

    8/4 힙

--- LEVEL2/더맵게/Solution.java
+++ LEVEL2/더맵게/Solution.java
@@ -1,8 +1,26 @@
 package LEVEL2.더맵게;
 
+import java.util.*;
+
 public class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        for(int s : scoville) {
+            heap.offer(s);
+        }
+        
+        while(heap.peek() <= K) {
+            if(heap.size() == 1) return -1;
+            int a = heap.poll();
+            int b = heap.poll();
+            
+            int result = a + (b * 2);
+            
+            heap.offer(result);
+            answer++;
+        }
         return answer;
     }
 }
