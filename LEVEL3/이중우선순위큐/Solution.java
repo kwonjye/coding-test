b2a17d9053490b6c5543df6f276b9fe8eac7efe3
KWONJYE <sheisdanmi@gmail.com>
2022-08-04 09:45:37 +0900

    8/4 힙

--- LEVEL3/이중우선순위큐/Solution.java
+++ LEVEL3/이중우선순위큐/Solution.java
@@ -1,5 +1,33 @@
 package LEVEL3.이중우선순위큐;
 
+import java.util.*;
+
 public class Solution {
-    
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(String oper : operations) {
+            if(oper.contains("I")) {
+                String[] iNum = oper.split(" ");
+                minHeap.add(Integer.parseInt(iNum[1]));
+                maxHeap.add(Integer.parseInt(iNum[1]));
+            }else if(oper.contains("D 1")) {
+                if(!maxHeap.isEmpty()) {
+                    int max = maxHeap.poll();
+                    minHeap.remove(max);
+                }
+            }else {
+                if(!minHeap.isEmpty()) {
+                    int min = minHeap.poll();
+                    maxHeap.remove(min);
+                }
+            }
+        }
+        int[] answer = new int[2];
+        if(!minHeap.isEmpty()) {
+            answer[0] = maxHeap.peek();
+            answer[1] = minHeap.peek();
+        }
+        return answer;
+    }
 }
